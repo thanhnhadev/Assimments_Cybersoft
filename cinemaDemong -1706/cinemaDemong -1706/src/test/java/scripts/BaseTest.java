922aeaf2fb8143f7a1b862676f523814b5a4ebe5
thanhnha <thanhnhadev@gmail.com>
2025-07-01 13:46:54 +0700

    report

---
+++ cinemaDemong -1706/cinemaDemong -1706/src/test/java/scripts/BaseTest.java
@@ -0,0 +1,63 @@
+package scripts;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
+//import java.util.logging.log4j.LogManager;
+//import java.util.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class BaseTest {
+
+    WebDriver driver;
+    static Logger logger = LogManager.getLogger(BaseTest.class);
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+//@Parameters("browser")// lay tham so tu testng.xml
+
+@BeforeMethod
+//public void setupTest(@Optional("chrome") String browser) {
+//        logger.info("susantest");
+//    if(browser.equalsIgnoreCase("chrome")) {
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+//    } else if (browser.equalsIgnoreCase("firefox")){
+//        WebDriverManager.firefoxdriver().setup();
+//        driver = new FirefoxDriver();
+//        }
+//    logger.info("mo trinh duyet");
+//
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        driver.manage().window().maximize();
+//        driver.get("https://demo1.cybersoft.edu.vn/");
+//    }
+public void setupTest(){
+         WebDriverManager.chromedriver().setup();
+         driver = new ChromeDriver();
+         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+         driver.manage().window().maximize();
+         driver.get("https://demo1.cybersoft.edu.vn/");
+         logger.info("đa truy cập thành công");
+
+}
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+        //driver.close();
+    }
+
+
+
+}
